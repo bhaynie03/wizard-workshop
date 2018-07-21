a8e65e23b7ed6a5ed85e130ca4cfa709037d4959
Brad Haynie <brad.lee.jest@gmail.com>
2018-07-21 00:19:28 -0700

    I moved the Navbar and added some preliminary content arround. I handled mostly cosmetic content arround today, and it isn't even close to being done, but I've started it. Next time I should finish up the basics of the master template and begin putting some functional content int there.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Jumbotron Template for Bootstrap</title>
+    <title>Wizard Workshop</title>
 
     <!-- Bootstrap core CSS -->
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -19,47 +19,16 @@
 
   <body>
 
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+    @include ('layouts.nav')
 
     <main role="main">
 
       <!-- Main jumbotron for a primary marketing message or call to action -->
       <div class="jumbotron">
         <div class="container">
-          <h1 class="display-3">Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          <h1 class="display-3">Welcome to the Wizard Workshop!</h1>
+          <p>Imagine a place where real magic begins. There may not be wands, cauldrons, or even pointy hats, but modern day wizards need a place to work too. Here is my personal Wizard Workshop, like a portable wizards castle, complete with arcane lore, useful tools, a dizzying array of projects in every conceivable stage of readiness, and occasionally . . .something fun. If you are me, please make sure to tidy things up on occasion. A clean workshop is a quick workshop. If you are not me, then please, have a look around. If you dabble in the arts, you may want to have a look in my libraries. You may find some of the my notes interesting. If you are curious about what wizards can do, have a look at my tools and projects. Most of them won't hurt you and am fairly sure you can't hurt them. And if you are interested in bying the services of wizard such as myself, please have a look at my portfolio. You should find all of the pertinent information on me there including an impressive array of my work.</p>
+          <p>I hope you find what you are looking for, and maybe something that you are not looking for, but as always, I hope you "Find the Magic."</p>
         </div>
       </div>
 
